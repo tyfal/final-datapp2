012383fa0980f92ca43ed212b9e9f508a6a36e04
tfalcoff <tfalcoff@gmail.com>
2017-11-26 20:35:15 -0600

    project root defined

--- datapp2/settings.py
+++ datapp2/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join('..', 'staticfiles')
+PROJECT_ROOT = '..'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
