b98ede6e096bbbbdbe113a589e930efeb51d2b40
tfalcoff <tfalcoff@gmail.com>
2017-11-26 21:21:06 -0600

    adding whitenoise storage to settings

--- datapp2/settings.py
+++ datapp2/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
