65bc24d87c727dbe656538420dc130a56e719c39
tfalcoff <tfalcoff@gmail.com>
2017-11-26 22:29:40 -0600

    change static dir

--- datapp2/settings.py
+++ datapp2/settings.py
@@ -132,7 +132,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
