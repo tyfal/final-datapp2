337a7d630e315aeb799844ee4260e3a75dd4462b
tfalcoff <tfalcoff@gmail.com>
2018-01-28 17:13:08 -0600

    adding urls redirect

--- datapp2/urls.py
+++ datapp2/urls.py
@@ -19,7 +19,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='folios2/')),
+    url(r'^$', RedirectView.as_view(url='folios2/')), 
     url(r'^folios2/', include('folios2.urls')),
     url(r'^admin/', admin.site.urls),
 ]
